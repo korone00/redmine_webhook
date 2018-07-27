5e27fceeb38b8870ff4756f2188fe102f491cd05
pododev <namsseng@podovat.com>
2018-07-27 22:54:24 +0900

    Add JANDI support

--- lib/redmine_webhook/webhook_listener.rb
+++ lib/redmine_webhook/webhook_listener.rb
@@ -42,15 +42,23 @@ module RedmineWebhook
       }.to_json
     end
 
+    $LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), '.'))
+    require 'jandi_api_wrapper'    
+    include Jandi_api_wrapper
+
     def post(webhooks, request_body)
       Thread.start do
         webhooks.each do |webhook|
           begin
+            if !webhook.url.match(/wh.jandi.com/).nil?
+              send_to_jandi(webhook.url,JSON.parse(request_body))
             Faraday.post do |req|
               req.url webhook.url
               req.headers['Content-Type'] = 'application/json'
               req.body = request_body
+             end
             end
+          Rails.logger.error JSON.parse(request_body)
           rescue => e
             Rails.logger.error e
           end
